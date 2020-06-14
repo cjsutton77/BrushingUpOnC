5bfd9244aa4904a05623923e88f522e12f8e4824
Christian Sutton <64376098+cjsutton77@users.noreply.github.com>
2020-06-13 22:13:25 -0500

    Add files via upload
    sol'n for HackerEarth question..

---
+++ linear_search.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int num,arr_len;
+	scanf("%d %d", &arr_len, &num);
+    //printf("%d\n", arr_len);
+    int arr[arr_len];
+	for (int i = 0; i < arr_len; i++) {
+		scanf("%d", &arr[i]);
+    }
+    for (int i = arr_len-1 ; i >= 0 ; i--){
+        if (num == arr[i]){
+            printf("%d",i+1);
+            break;
+        }
+        if (i == 0){
+            printf("%d",-1);
+        }
+    }
+
+	return 0;
+}
